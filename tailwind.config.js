4bd296699a1e2feba6ec614c1ebdf920055d9f1c
luxueqi26 <luxueqi2635@gmail.com>
2026-07-21 16:58:20 +0800

    Initial commit - Next.js Cloudflare

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './app/**/*.{js,jsx}',
+    './components/**/*.{js,jsx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          50: '#edf2ff',
+          100: '#dbe4ff',
+          200: '#bac8ff',
+          300: '#91a7ff',
+          400: '#748ffc',
+          500: '#5c7cfa',
+          600: '#4263eb',
+          700: '#3b5bdb',
+          800: '#364fc7',
+          900: '#2b3fa0',
+        }
+      }
+    }
+  },
+  plugins: [],
+}
